50882b35a0aff9bc1d6510bbca6ffd643e9f7328
Keith Loughnane <keith.loughnane@gmail.com>
2016-10-10 20:22:51 +0300

    Data passing in and out, working with data, some test code changing the JSON array. Distance calculation working with hard coded values. I think I only need to connect the dots now, and boiler plate. Taking a break.

--- routes/activeLocations.js
+++ routes/activeLocations.js
@@ -6,14 +6,24 @@ var router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/:date', function(req, res, next) {
 
     //Takes UNIX Time, returns active locations (between startTime and expireTime)
 
-    console.log(req.params.unixtime);
+    console.log(req.params.date);
     //res.send('activeLocations');
-
     locations = require('../data.json');
+
+    locations.forEach(function(item, index, object)
+        {
+            if(item.id%2 > 0)
+            {
+                locations.splice(index, 1)
+            }
+
+        }
+    );
+
 
     res.send(JSON.stringify(locations));
 
--- routes/getDistance.js
+++ routes/getDistance.js
@@ -5,14 +5,43 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('getDistance');
+router.get('/:coords', function(req, res, next) {
 
 
+    console.log(req.params.coords);
+    //res.send('activeLocations');
 
+    var dist = distance(26.4325,51.3345,26.4444,51.2633,"K")
+    console.log(dist + "KM");
+
+
+    //locations = require('../data.json');
+
+    //res.send(JSON.stringify(locations));
 
 
 
 });
+
+/*
+I got this from http://www.geodatasource.com
+ */
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    return dist
+}
+
+
+
 
 module.exports = router;
