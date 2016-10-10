845e5772eb8cc80ccedb8377dca258513aa437e3
Keith Loughnane <keith.loughnane@gmail.com>
2016-10-11 00:18:25 +0300

    Finding locations with for a particular UNIX date time is working. Needs to check for nulls though. That's not yet implemented.

--- routes/activeLocations.js
+++ routes/activeLocations.js
@@ -10,18 +10,59 @@ router.get('/:date', function(req, res, next) {
 
     //Takes UNIX Time, returns active locations (between startTime and expireTime)
 
-    console.log(req.params.date);
+
     //res.send('activeLocations');
     locations = require('../data.json');
 
-    locations.forEach(function(item, index, object)
-        {
-            if(item.id%2 > 0)
-            {
-                locations.splice(index, 1)
-            }
+
+    /* Just testing for now */
+
+    var currentDate = parseInt(req.params.date.toString().substring(4));
+    while (currentDate < 1000000000000)
+    {
+        /* I could have juse *1000 but I wanted to get the edge cases*/
+        currentDate*=10;
+    }
+
+    //currentDate = currentDate.toString();
+    //console.log("Date > " + currentDate);
+
+
+    //locations.forEach(function(item, index, object)
+      //  {
+    // The for each was more trouble than it was word. Elements moveing around as their removed caused issues.
+
+    for(i = 0 ; i < locations.length;)
+    {
+
+
+        var testCRDate = parseInt(new Date(locations[i].date_created).getTime());
+        var testDSDate = parseInt(new Date(locations[i].date_disabled).getTime());
+        console.log("ID > " + locations[i].id);
+        console.log("Created > " + testCRDate);
+        console.log("Date > " + currentDate);
+        console.log("Disabled > " + testDSDate);
+
+
+        if (testCRDate < currentDate && testDSDate > currentDate) {
+            console.log((testCRDate < currentDate).toString() + "," + (testDSDate > currentDate).toString());
+            console.log("Passed Test, keeping");
+            i++;
+
         }
-    );
+        else {
+            console.log((testCRDate < currentDate).toString() + "," + (testDSDate > currentDate).toString());
+            console.log("Failed test, removing");
+            locations.splice(i, 1);
+
+            //Don't increment of removing the whole array drops down by one which results in more or less the same effect.
+        }
+
+        console.log("----------------");
+    }
+      //  }
+
+    //);
 
     res.send(JSON.stringify(locations));
 });
